822da84e4cb64cfb8c5788ac638d342b28055d02
Centauri07 <magnus.centauri07@gmail.com>
2022-08-13 21:23:20 +0800

    added jda dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("net.dv8tion:JDA:5.0.0-alpha.17")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
