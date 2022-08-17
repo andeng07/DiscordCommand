6153fec6383ae6e7538a8839b4156271c2d9f488
Centauri07 <magnus.centauri07@gmail.com>
2022-08-17 10:17:48 +0800

    added maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.7.10"
     java
+    `maven-publish`
 }
 
 group = "me.centauri07.dc"
@@ -15,10 +16,22 @@ dependencies {
 
     implementation("net.dv8tion:JDA:5.0.0-alpha.17")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "me.centauri07.dc"
+            artifactId = "DiscordCommand"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
 }
